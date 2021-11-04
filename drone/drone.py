9026f28c4082e06b2ceb88d5fc9bfa303a15ef19
Steven Davenport <sldavenport1996@gmail.com>
2021-11-04 12:23:41 +0000

    More methods for drone controller + more ground work in main

--- drone/drone.py
+++ drone/drone.py
@@ -2,11 +2,13 @@
 from djitellopy import Tello 
 
 
+
 class DroneController:
     def __init__(self) -> None:
         self.tello = Tello()
 
 
+    # Connect the controller to physical tello - optional turn on stream
     def connect(self, streamon=True):
         self.tello.connect()
         if streamon:
@@ -14,8 +16,19 @@ class DroneController:
             self.tello.streamon()
         
         
+    # Flight test for Tello
     def test_drone_movement(self):
         print(self.tello.get_battery())
         self.tello.takeoff()
         self.tello.rotate_clockwise(50)
         self.tello.land()
+
+
+    # Returns the frame captured by tello
+    def get_frame(self):
+        return self.tello.get_frame_read().frame
+
+
+    # Returns the battery level of tello
+    def get_battery(self):
+        return self.tello.get_battery()
--- main.py
+++ main.py
@@ -4,18 +4,44 @@ from drone.drone import DroneController
 # User tracking module
 from userTracking.userTracking import UserTracking
 
+# CV2 used to display drone camera feed
+import cv2
+
+# Time used to calculate FPS
+import time
+
 
 # Main function which connects the modules and drives the program
 def main():
 
     # Create drone control object
+    # Connect it to the physical drone
     drone = DroneController()
-
-    # Connect the control module to physical drone
     drone.connect()
 
-    # flight and vision test
-    drone.test_drone_movement()
+    # Video loop
+    while True:
+        # FPS Calculations
+        start_time = time.time()
+
+        # Get the frame from tello -> transform
+        frame = drone.get_frame()
+
+        # FPS Calculations
+        # Display FPS
+        fps = 1.0 / (time.time() - start_time)
+        fps_string = 'FPS: ' + str(int(fps))
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        cv2.putText(frame, fps_string, (800, 80), font, 1, (0, 102, 34), 2, cv2.LINE_AA)
+
+        # Display Frame 
+        cv2.imshow('Drone Vision', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    
+    # Close the viewing window
+    cv2.destroyAllWindows()
+
 
 
 if __name__ == '__main__':
