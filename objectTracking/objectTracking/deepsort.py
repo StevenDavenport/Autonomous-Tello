11164c772e3b4c59714683df155e80089f89b064
sldavenport1996@gmail.com <sldavenport1996@gmail.com>
2021-11-15 16:06:48 +0000

    Deepsort returns dictionary of tracking data

--- objectTracking/objectTracking/deepsort.py
+++ objectTracking/objectTracking/deepsort.py
@@ -1,10 +1,22 @@
 from deep_sort_realtime.deepsort_tracker import DeepSort
+from numpy.core.numeric import identity
 
 class DeepSORT:
     def __init__(self):
         self.tracker = DeepSort(max_age=30, nn_budget=70, override_track_class=None)
         
     def track(self, detections, frame):
-        detections = self.tracker.update_tracks(raw_detections=detections, frame=frame)
-        return detections
+        tracks = self.tracker.update_tracks(raw_detections=detections, frame=frame)
+        return self.format_detections(tracks, detections)
+
+    def format_detections(self, tracks, detections):
+        tracked_detections = list()
+        for detection, track in zip(detections, tracks):
+            tracked_detections.append(dict(
+                class_label=detection[2],
+                confidence_score=detection[1],
+                object_identity=track.track_id,
+                location=[track.mean[0], track.mean[1], track.mean[2], track.mean[3]]
+            ))
+        return tracked_detections
 
